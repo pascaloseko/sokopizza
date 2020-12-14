96453a37183f97283f2eab2b67e7973159b6ef16
Pascal Oseko <pascal.ondieki21@gmail.com>
2020-12-14 11:22:06 +0300

    add: model structs

--- order/models.py
+++ order/models.py
@@ -7,7 +7,7 @@ quant = Decimal('0.01')
 
 class Size(models.Model):
     name = models.CharField(max_length=30)
-    price = models.DecimalField(max_digits=4, decimal_places=2, default=0.00)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
 
     def __str__(self):
         return self.name
@@ -19,10 +19,19 @@ TOPPING_TYPE = (
 )
 
 
+class ToppingType(models.Model):
+    name = models.CharField(choices=TOPPING_TYPE, max_length=20)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+    size = models.ForeignKey(Size, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
 class Topping(models.Model):
     name = models.CharField(max_length=30)
-    price = models.DecimalField(max_digits=4, decimal_places=2, default=0.00)
-    topping_type = models.CharField(choices=TOPPING_TYPE, max_length=20)
+    topping_type = models.ForeignKey(
+        ToppingType, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -31,20 +40,7 @@ class Topping(models.Model):
 class Pizza(models.Model):
     size = models.ForeignKey(Size, null=True, on_delete=models.CASCADE)
     toppings = models.ManyToManyField(Topping)
-    price = models.DecimalField(max_digits=4, decimal_places=2, default=0.00)
-
-    def save(self, *args, **kwargs):
-        if not Pizza.objects.filter(id=self.id):
-            super(Pizza, self).save(*args, **kwargs)
-        unit_price = Decimal('0.00')
-        if self.size:
-            unit_price = self.size.price
-            for topping in self.toppings.all():
-                if topping.price:
-                    unit_price += topping.price
-        self.price = decimal.Decimal(str(unit_price)).quantize(quant)
-        print(unit_price)
-        super(Pizza, self).save(*args, **kwargs)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
 
     def __str__(self):
         if self.size.name:
@@ -52,7 +48,8 @@ class Pizza(models.Model):
         name = 'Pizza'
         for topping in self.toppings.all():
             if topping.name:
-                name = name + ', ' + topping.name
+                name = name + ', ' + topping.name + \
+                    ', ' + str(topping.topping_type.price)
         return name
 
 
